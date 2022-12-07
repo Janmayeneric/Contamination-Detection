9b857a3bfaf3b002106cfad9d41d0d42b3bc9c8c
Janmayeneric <43655740+Janmayeneric@users.noreply.github.com>
2022-12-07 14:19:25 -0500

    update the practical code without recursion

--- Approximate-Matching/match.py
+++ Approximate-Matching/match.py
@@ -1,4 +1,51 @@
 from functools import lru_cache
+
+
+class EditDistance:
+    expect = -1
+    def match(self):
+    
+        max_iy = self.len_y
+        
+        # first column is from 0 to len(y)
+        pre_column = range(self.len_y + 1)
+        
+        for i in range(self.len_x):
+            # first row is always the zero
+            current_column = [0]
+            
+            for j in range(self.len_y):
+                delt = 1 if self.x[i] != self.y[j] else 0
+                
+                current_column.append(min(pre_column[j]+delt,pre_column[j+1]+1,current_column[j]+1))
+                
+            self.min_edit = min(self.min_edit,current_column[-1])
+
+            pre_column = current_column
+            if self.expect > 0:
+                if self.min_edit <= self.expect:
+                    break
+            
+            
+        
+    def __init__(self,x,y):
+        
+        # swap the x and y, the basic rule, y must be shorter than x
+        if len(x) > len(y):
+            self.x = x
+            self.y = y
+        else:
+            self.y = x
+            self.x = y
+        
+        self.len_x = len(self.x)
+        self.len_y = len(self.y)
+        
+        self.min_edit = self.len_y
+
+        
+        
+        
 '''
 This class simply use the edit distance with dynamic programming to solve the approximate matching problem
 
@@ -6,9 +53,9 @@ Some important issue:
 editDistance(x,y)
 length of x must be longer than y
 '''
+        
 
-
-class EditDistance:
+class EditDistanceRecursive:
 
     @lru_cache(None)
     def match(self,ix, iy):
@@ -55,5 +102,15 @@ def read_fasta(path):
     while len(line) > 0:
         res.append(f.readline().rstrip())
         line = f.readline()
+    f.close()
+    return ''.join(res)
 
+def read_file(path):
+    f = open(path,'r')
+    line = f.readline().rstrip()
+    res = []
+    while len(line) > 0 :
+        res.append(line)
+        line = f.readline()
+    f.close()
     return ''.join(res)
--- Approximate-Matching/test.py
+++ Approximate-Matching/test.py
@@ -1,10 +1,17 @@
-import sys
-from match import EditDistance, read_fastq, read_fasta
-import threading
-threading.stack_size(2**26)
-sys.setrecursionlimit(10**6)
-a = read_fasta('output.fastq')
-b = read_fastq('SRR6756023.fastq')
 
-ed = EditDistance(b,a)
-threading.Thread(target = ed.find()).start()
+from match import EditDistance, read_fastq, read_fasta, read_file, EditDistanceRecursive
+
+
+
+p = read_file('p1.txt')
+t = read_file('t1.txt')
+
+ed = EditDistance(p,t)
+ed.expect = 1000
+ed.match()
+
+print(ed.min_edit)
+
+
+
+
